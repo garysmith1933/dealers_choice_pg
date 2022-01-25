aa056d384a3416360cfed1561dca9f213a735093
Gary <garysmith193358@gmail.com>
2022-01-25 22:18:47 +0000

    worked on generateManagementTree but dont understand it.

--- acme-employees/acme-employees.js
+++ acme-employees/acme-employees.js
@@ -130,67 +130,92 @@ console.log(findManagementChainForEmployee(findEmployeeByName('shep Jr.', employ
 spacer('');
 
 
-// spacer('generateManagementTree')
-// //given a list of employees, generate a tree like structure for the employees, starting with the employee who has no manager. Each employee will have a reports property which is an array of the employees who report directly to them.
-// console.log(JSON.stringify(generateManagementTree(employees), null, 2));
-// /*
-// {
-//   "id": 1,
-//   "name": "moe",
-//   "reports": [
-//     {
-//       "id": 2,
-//       "name": "larry",
-//       "managerId": 1,
-//       "reports": [
-//         {
-//           "id": 4,
-//           "name": "shep",
-//           "managerId": 2,
-//           "reports": [
-//             {
-//               "id": 8,
-//               "name": "shep Jr.",
-//               "managerId": 4,
-//               "reports": []
-//             }
-//           ]
-//         }
-//       ]
-//     },
-//     {
-//       "id": 3,
-//       "name": "curly",
-//       "managerId": 1,
-//       "reports": [
-//         {
-//           "id": 5,
-//           "name": "groucho",
-//           "managerId": 3,
-//           "reports": [
-//             {
-//               "id": 6,
-//               "name": "harpo",
-//               "managerId": 5,
-//               "reports": []
-//             }
-//           ]
-//         }
-//       ]
-//     },
-//     {
-//       "id": 99,
-//       "name": "lucy",
-//       "managerId": 1,
-//       "reports": []
-//     }
-//   ]
-// }
-// */
-// spacer('');
+spacer('generateManagementTree')
+//given a list of employees, generate a tree like structure for the employees, starting with the employee who has no manager. Each employee will have a reports property which is an array of the employees who report directly to them.
+
+function generateManagementTree(employees) {
+  let boss = []
+  
+  for (let i = 0; i < employees.length; i++) {
+    let worker = employees[i]
+    
+    if (!worker.managerId) {
+      boss = worker;
+    }
+    
+    // the boss has a reports property that targets the rest of the tree. but I do not how to proceed from here.
+    // boss.reports = ???
+    
+  }
+  
+
+  
+}
+console.log(JSON.stringify(generateManagementTree(employees), null, 2));
+/*
+{
+  "id": 1,
+  "name": "moe",
+  "reports": [
+    {
+      "id": 2,
+      "name": "larry",
+      "managerId": 1,
+      "reports": [
+        {
+          "id": 4,
+          "name": "shep",
+          "managerId": 2,
+          "reports": [
+            {
+              "id": 8,
+              "name": "shep Jr.",
+              "managerId": 4,
+              "reports": []
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "id": 3,
+      "name": "curly",
+      "managerId": 1,
+      "reports": [
+        {
+          "id": 5,
+          "name": "groucho",
+          "managerId": 3,
+          "reports": [
+            {
+              "id": 6,
+              "name": "harpo",
+              "managerId": 5,
+              "reports": []
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "id": 99,
+      "name": "lucy",
+      "managerId": 1,
+      "reports": []
+    }
+  ]
+}
+*/
+spacer('');
+
+
 
 // spacer('displayManagementTree')
 // //given a tree of employees, generate a display which displays the hierarchy
+
+// I do not know..
+
+
 // displayManagementTree(generateManagementTree(employees));/*
 // moe
 // -larry
